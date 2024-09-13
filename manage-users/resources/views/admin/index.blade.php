2febc9268fd5497d40c33a9ae481a9433d97b1c4
mahdirnma <rahnamamahdi78@gmail.com>
2024-09-13 12:42:43 +0330

    create users panel

--- manage-users/resources/views/admin/index.blade.php
+++ manage-users/resources/views/admin/index.blade.php
@@ -1,1 +1,18 @@
-<?php
+@extends('layout.app')
+@section('title')
+    dashboard
+@endsection
+@section('content')
+    <div class="w-full h-[88%] bg-gray-200 flex flex-row-reverse pr-20 pt-10">
+{{--
+        <div class="w-1/5 h-48 flex flex-col justify-center items-center ml-16 bg-white rounded-xl">
+            <h2 class="mb-8">تعداد استاد ها</h2>
+            <p class="text-5xl font-light">{{$professors}}</p>
+        </div>
+        <div class="w-1/5 h-48 flex flex-col justify-center items-center bg-white rounded-xl ml-16 ">
+            <h2 class="mb-8">تعداد دوره ها</h2>
+            <p class="text-5xl font-light">{{$courses}}</p>
+        </div>
+--}}
+    </div>
+@endsection
--- manage-users/resources/views/admin/user/index.blade.php
+++ manage-users/resources/views/admin/user/index.blade.php
@@ -1,1 +1,54 @@
-<?php
+@extends('layout.app')
+@section('title')
+    sections
+@endsection
+@section('content')
+    <div class="w-full h-[88%] bg-gray-200 flex items-center justify-center">
+        <div class="w-[90%] h-5/6 bg-white rounded-xl pt-3 flex flex-col items-center">
+            <div class="w-[90%] h-1/5 flex justify-between items-center border-b">
+                <a href="{{--{{route('section.create')}}--}}" class="px-10 py-3 rounded-xl font-light text-white bg-gray-800">add user +</a>
+                <h2 class="text-xl">users</h2>
+            </div>
+            <div class="w-[90%] h-3/5 flex justify-between items-center">
+                <table class="w-full min-h-full border border-gray-400">
+                    <thead>
+                    <tr class="h-12 border border-gray-400 border-b-2 border-b-gray-400">
+                        <td class="text-center">حذف</td>
+                        <td class="text-center">ویرایش</td>
+                        <td class="text-center">category</td>
+                        <td class="text-center">username</td>
+                        <td class="text-center">role</td>
+                        <td class="text-center">gender</td>
+                        <td class="text-center">age</td>
+                        <td class="text-center">name</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($users as $user)
+                        <tr>
+                            <td class="text-center">
+                                <form action="{{--{{route('section.destroy',compact('section'))}}--}}" method="post">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="text-green-600">delete</button>
+                                </form>
+                            </td>
+                            <td class="text-center">
+                                <form action="{{--{{route('section.update',compact('section'))}}--}}" method="get">
+                                    @csrf
+                                    <button type="submit" class="text-cyan-600">update</button>
+                                </form>
+                            </td>
+                            <td class="text-center">{{$user->category->title}}</td>
+                            <td class="text-center">{{$user->username}}</td>
+                            <td class="text-center">{{$user->role}}</td>
+                            <td class="text-center">{{$user->gender}}</td>
+                            <td class="text-center">{{$user->age}}</td>
+                            <td class="text-center">{{$user->name}}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+@endsection
