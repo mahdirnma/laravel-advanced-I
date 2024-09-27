216a7d3e47b406086dacf25c12941b8b984b6dee
mahdirnma <rahnamamahdi78@gmail.com>
2024-09-27 19:17:21 +0330

    create register panel and build register process

--- manage-title/app/Http/Controllers/AuthController.php
+++ manage-title/app/Http/Controllers/AuthController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreUserLoginRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -15,5 +18,30 @@ class AuthController extends Controller
         }else{
             return to_route('login.show');
         }
+    }
+
+    public function register(StoreUserRequest $request)
+    {
+        $name=$request->name;
+        $username=$request->username;
+        $password=$request->password;
+        $confirmPassword=$request->confirmPassword;
+        $role=$request->role;
+        $user=User::create([
+            'name'=>$name,
+            'username'=>$username,
+            'password'=>Hash::make($password),
+            'role'=>$role,
+        ]);
+        if($user){
+            Auth::login($user);
+            return to_route('title.index');
+        }else{
+            return to_route('register.show');
+        }
+    }
+    public function logout(){
+        Auth::logout();
+        return to_route('login.show');
     }
 }
--- manage-title/resources/views/auth/register.blade.php
+++ manage-title/resources/views/auth/register.blade.php
@@ -5,21 +5,23 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>signin</title>
+    <title>register</title>
     @vite('resources/css/app.css')
 </head>
 <body>
 <div class="w-svw h-svh flex justify-center items-center bg-gray-200">
     <div class="w-2/6 h-5/6 bg-white rounded-xl flex flex-col items-center justify-start">
-        <h1 class="text-3xl pt-8">ثبت نام</h1>
-        <form action="{{route('register')}}" method="post" class="w-5/6 flex flex-col items-end justify-start mt-5">
+        <h1 class="text-3xl pt-2">ثبت نام</h1>
+        <form action="{{route('register')}}" method="post" class="w-5/6 flex flex-col items-end justify-start">
             @csrf
-            <label for="name" class="mt-5">نام</label>
+            <label for="name" class="mt-2">نام</label>
             <input type="text" name="name" id="name" class="w-full h-10 bg-gray-100 rounded mt-3">
             <label for="username" class="mt-5">نام کاربری</label>
-            <input type="text" name="username" id="username" class="w-full h-10 bg-gray-100 rounded mt-3">
+            <input type="text" name="username" id="username" class="w-full h-10 bg-gray-100 rounded mt-1">
+            <label for="role" class="mt-5">نقش</label>
+            <input type="number" name="role" min="1" max="10" id="role" class="w-full h-10 bg-gray-100 rounded mt-1">
             <label for="password" class="mt-5">رمز عبور</label>
-            <input type="password" name="password" id="password" class="w-full h-10 bg-gray-100 rounded mt-3">
+            <input type="password" name="password" id="password" class="w-full h-10 bg-gray-100 rounded mt-1">
             <label for="confirmPassword" class="mt-5">تکرار رمز عبور</label>
             <input type="password" name="confirmPassword" id="confirmPassword" class="w-full h-10 bg-gray-100 rounded mt-3">
             <input type="submit" value="ثبت نام" class="w-full h-12 cursor-pointer text-gray-100 bg-gray-600 rounded mt-8">
