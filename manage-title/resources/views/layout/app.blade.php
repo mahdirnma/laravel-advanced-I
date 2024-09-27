f43dd6d542e1077726cd8859307e38ed7fc962d7
mahdirnma <rahnamamahdi78@gmail.com>
2024-09-27 19:21:02 +0330

    build logout process

--- manage-title/resources/views/layout/app.blade.php
+++ manage-title/resources/views/layout/app.blade.php
@@ -19,7 +19,7 @@
                 </ul>
             </nav>
         </div>
-        <form action="{{--{{route('logout')}}--}}" method="post">
+        <form action="{{route('logout')}}" method="post">
             @csrf
             <button type="submit" class="text-red-700 font-bold"><- logout</button>
         </form>
